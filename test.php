49e77fff31d20bd68c25ca683815ece4d7e05c8d
Стефан Йовев <stefanyovev@users.noreply.github.com>
2018-12-10 23:20:26 +0200

    Update test.php

--- test.php
+++ test.php
@@ -25,7 +25,6 @@ Bdir/Afile.ctime
 Bdir/Bfile
 Cfile
 
-empty( p( Cfile ) ) -> True
 len( p( Bdir ) ) -> 3
 p( Bdir )[0] -> 'file'
 p( Bdir )[1] -> 'content of Afile'
@@ -38,13 +37,13 @@ $M =
    [background, id => background],
    [dir, id => Adir],
    [dir, id => Bdir,
-      [file, id => Afile, ctime => '2018', 'content of Afile'],
-      [file, id => Bfile], // empty Bfile
-      'text' ], // text node in Bdir
+      [file, id => Bfile, ctime => '2018', 'content of Afile'],
+      [file, id => Bfile2],
+      'text' ],
    [file, id => Cfile] ];
 
 
-   function ui( & $x, $l=0 ){
+   function xml( & $x, $l=0 ){
       $p = str_repeat( ' ', 3 + $l*3 ) . '-->';
       $sol = ( $l ? $p : '      ' );
       $eol = ( $l ? '<!--' : '' ) . "\n";
@@ -53,7 +52,7 @@ $M =
       echo $sol . "<{$x[0]}$a";
       if( !isset( $x[1] ) ) echo " /><!--\n";
       else{ echo "><!--\n";
-         for( $i=1; isset( $x[$i] ); $i++ ) ui( $x[$i], $l+1 );
+         for( $i=1; isset( $x[$i] ); $i++ ) xml( $x[$i], $l+1 );
          echo "$p</{$x[0]}>" . $eol; } }
 
    function & p( $x = '' ){ global $M; $p = & $M; if( $x ){
@@ -63,17 +62,17 @@ $M =
          if( !$found ) return NULL; } }
       return $p; }
 
-   function newid( $x ){ $y = 2; while( p( $x ) ) $x .= $y;
-      return $x; }
+   function newid( $x ){ $n = 1; $r = $x; while( p( $r ) ) $r = $x . ++$n;
+      return $r; }
 
    function valid( $data, $type ){ switch( $type ){      
       case id: return $data && is_string( $data ) && $data[0] != '-' && $data[0] != '_' &&
          !preg_match( '/\A[0-9\-_]+\z/', $data ) && preg_match( '/\A[a-z0-9\-_]+\z/', $data ); }
       return false; }
 
-//var_dump( preg_match( '/\A[0-9_]\z/', 'asd' ) );
 
-//foreach( ['a_66_hhh','66_ways','66-way','5','55','-55','-_','_-','asd','-rr-'] as $x ) echo ( valid( $x, id ) ? 'Y' : '-' ) . " '$x' \n";
-//var_dump( newid( 'Bdir/Bfile' ) );
-ui($M);
+   p( Bdir )[] = [tag, 'text'];
+
+   xml($M);
+
  ?>
